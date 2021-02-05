7898a224c89ba792a1202cc14d8062964f0c0e80
thinhlegithub <thinhle.au@gmail.com>
2021-02-05 11:03:06 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const api={
-    key: 'e031ee04d2084d1d03a78f3fb53e3278',
-    base_url: 'https://api.openweathermap.org/data/2.5/'
+    key: 'aae6c1208cf04f6492d01510210402',
+    base_url: 'http://api.weatherapi.com/v1/'
 }
 const search_box=document.querySelector(".search-box");
 const form = document.getElementById("form");
@@ -35,9 +35,7 @@ function showPosition(position){
     
 }
 function loadCurrentPossionWeather(latitude, longitude){    
-    //let api_url=api.base_url+`weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${api.key}`;
-    //let api_url=`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&cnt=7&appid=${api.key}`
-    let api_url=`https://api.openweathermap.org/data/2.5/forecast/?lat=${latitude}&lon=${longitude}&cnt=40&units=metric&appid=${api.key}`
+    let api_url=api.base_url+`forecast.json?q=${latitude},${longitude}&days=3&key=${api.key}`;
     console.log(api_url);
     fetch(api_url)
     .then(function(weather){
@@ -47,80 +45,7 @@ function loadCurrentPossionWeather(latitude, longitude){
 
 
 function getWeatherResults(search_input){
-    let api_url=api.base_url+`forecast/?q=${search_input}&cnt=40&units=metric&appid=${api.key}`;
-    console.log(api_url);
-    notificationEl.style.display='none';
-    fetch(api_url)
-    .then(function(forecast){
-        return forecast.json();
-    })
-    .then(displayResults)
-}
-
-function displayResults(forecast){
-    
-    showDate();
-
-    const currentWeather = forecast.list[0];
-
-    let city = document.querySelector('.location .city');
-    city.innerText = `${forecast.city.name}, ${forecast.city.country}`;
-
-    let temp=document.querySelector(".temp");
-    temp.innerHTML=`${Math.floor(currentWeather.main.temp)}<span>°c</span>`;
-
-    let iconId=document.querySelector(".iconId");
-    iconId.innerHTML=`<img src="icons/${currentWeather.weather[0].icon}.png" />`;
-
-    let weather_description=document.querySelector('.weather-description p');
-    weather_description.innerText=currentWeather.weather[0].description;   
-
-    let hi_low=document.querySelector(".hi-low");
-    hi_low.innerText=`${Math.floor(currentWeather.main.temp_min)}°c / ${Math.floor(currentWeather.main.temp_max)}°c`;
-
-
-    let sub=document.querySelector('sub');
-    let eachDayForcastEl='';
-    let dayForcastEls=forecast.list;
-    
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day=''
-    //let d=new Date();
-    let d='';
-    var i=7;
-    var j=0;
-    var utcString='';
-    for(i; i< dayForcastEls.length; i+=8)
-    {       
-        //build days loop the day of weeks
-          /*j=d.getDay()+i;
-          if(j>=7)
-            {
-                j=j-8;
-                j++;
-            }
-        */
-            d=new Date(forecast.list[i].dt*1000);
-            console.log(d);
-            day = days[d.getDay()];
-            if(i===dayForcastEls.length){
-                i=i-3;
-                return;
-            }
-            eachDayForcastEl+=`<div class="forcast">
-                    <div class="date">${day}</div>
-                    <div class="forcast_iconId"><img src="icons/${forecast.list[i].weather[0].icon}.png" alt=""></div>
-                    <div class="forcast_temp">${Math.floor(forecast.list[i].main.temp)}<span>°c</span></div>
-                    <div class="forcast_weather-description"><p>${forecast.list[i].weather[0].description}</p></div>
-                    <div class="forcast_hi-low">${Math.floor(forecast.list[i-3].main.temp_min)}°c / ${Math.floor(forecast.list[i].main.temp_max)}°c</div>
-            </div>`;
-    }
-    sub.innerHTML=eachDayForcastEl;
-}
-
-/*
-function getWeatherResults(search_input){
-    let api_url=api.base_url+`forecast/?q=${search_input}&cnt=7&units=metric&appid=${api.key}`;
+    let api_url=api.base_url+`forecast.json?q=${search_input}&days=3&key=${api.key}`;
     console.log(api_url);
     notificationEl.style.display='none';
     fetch(api_url)
@@ -129,37 +54,49 @@ function getWeatherResults(search_input){
     })
     .then(displayResults)
 }
-function getWeatherResults(search_input){
-    let api_url =api.base_url+`weather?q=${search_input}&units=metric&appid=${api.key}`;
-    notificationEl.style.display='none';
-    fetch(api_url)
-    .then(function(weather){
-        return weather.json();
-    })
-    .then(displayResults)
-}
-
 function displayResults(weather){
     
     showDate();
 
     let city = document.querySelector('.location .city');
-    city.innerText = `${weather.name}, ${weather.sys.country}`;
+    city.innerText = `${weather.location.name}, ${weather.location.country}`;
 
     let temp=document.querySelector(".temp");
-    temp.innerHTML=`${Math.floor(weather.main.temp)}<span>°c</span>`;
+    temp.innerHTML=`${Math.round(weather.current.temp_c)}<span>°c</span>`;
 
     let iconId=document.querySelector(".iconId");
-    iconId.innerHTML=`<img src="icons/${weather.weather[0].icon}.png" />`;
+    iconId.innerHTML=`<img src="${weather.current.condition.icon}" />`;
 
     let weather_description=document.querySelector('.weather-description p');
-    weather_description.innerText=weather.weather[0].description;   
+    weather_description.innerText=weather.current.condition.text;   
 
     let hi_low=document.querySelector(".hi-low");
-    hi_low.innerText=`${Math.floor(weather.main.temp_min)}°c / ${Math.floor(weather.main.temp_max)}°c`;
-}
-*/
+    hi_low.innerText=`${Math.floor(weather.forecast.forecastday[0].day.mintemp_c)}°c / ${Math.round(weather.forecast.forecastday[0].day.maxtemp_c)}°c`;
+    
 
+    let sub=document.querySelector('sub');
+    let eachDayForcastEl='';
+    let daysForcast=weather.forecast.forecastday;
+    
+    
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day='';
+    let d='';
+    
+ 
+    var i=0;   
+    for(i; i< daysForcast.length; i++)
+    {       d=new Date(daysForcast[i].date);
+            day = days[d.getDay()];
+            eachDayForcastEl+=`<div class="forecast">
+                    <div class="date">${day}</div>
+                    <div class="forcast_iconId"><img src="${daysForcast[i].day.condition.icon}"/></div>
+                    <div class="forcast_weather-description"><p>${daysForcast[i].day.condition.text}</p></div>
+                    <div class="forcast_hi-low">${Math.floor(daysForcast[i].day.mintemp_c)}°c / ${Math.round(daysForcast[i].day.maxtemp_c)}°c</div>
+            </div>`;
+    }
+    sub.innerHTML=eachDayForcastEl;
+}
 function buildDate(d){
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
